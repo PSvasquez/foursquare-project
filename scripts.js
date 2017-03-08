cd893794a289d84b19b53efbb37f456186e77358
Patricia Santana <psantana.student@hsfi.us>
2017-03-08 18:06:02 +0000

    added to the javascript

--- scripts.js
+++ scripts.js
@@ -3,5 +3,13 @@ $(document).ready(function (){
  $("button").on("click", function(){
      var location = $('input').val();
      var url = "https://api.foursquare.com/v2/venues/search?client_id=2SNBS2ZISJKMUIIZLCWY4FHA14Y0HYRR5T5OIOTFPVVWKHXM&client_secret=SE3BEPYPGXWUAPQWF1ZBWQ2GFMOEC4G44FPHHVP4OMCPXLT0&v=20151209&near="+ location
+     + ""
 }
+
     });
+    console.log(url)
+     $.get(url, function(data){
+         var data = data.;
+    $("#food-results").html(data+"");
+    console.log(data)
+     }
